db6b06c9416ec7a246adef7ced77513636d1dd22
lquezad <lquezad@vt.edu>
2026-09-24 13:09:45 -0400

    DateTimeUtil change

--- DateTimeUtil.java
+++ DateTimeUtil.java
@@ -1,25 +1,57 @@
-import java.time;
-import java.time.format;
+import java.time.*;
+import java.time.format.*;
 
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author lquezadr
+ *  @version Sep 24, 2026
+ */
 public class DateTimeUtil
 {
 
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @param date
+     * @param time
+     * @return
+     */
     public LocalDateTime parseDateTime(String date, String time)
     {
-        ///
+        return null;
     }
 
 
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @param dt
+     * @param now
+     * @return
+     */
     public boolean isPast(LocalDateTime dt, LocalDateTime now)
     {
-        ///
+        return false;
     }
 
 
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @param start1
+     * @param duration1
+     * @param start2
+     * @param duration2
+     * @return
+     */
     public boolean overlaps (LocalDateTime start1, int duration1,
         LocalDateTime start2, int duration2)
     {
-        ///
+        return false;
     }
 
 
