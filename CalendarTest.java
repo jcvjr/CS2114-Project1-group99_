aef02dcd7090d6b44b98e1b437ec609f56ef6095
Nick Marchese <ywgnick@gmail.com>
2026-09-24 23:32:43 -0400

    Add CalendarTest

---
+++ CalendarTest.java
@@ -0,0 +1,113 @@
+/**
+ * Tests the Calendar class.
+ * 
+ * @author Nicolas Marchese (nmarchese)
+ * @version 09.24.2026
+ */
+public class CalendarTest extends student.TestCase {
+
+    private Calendar calendar;
+
+    /**
+     * Creates a new Calendar.
+     */
+    public void setUp() {
+        calendar = new Calendar();
+    }
+
+
+    /**
+     * Tests adding an event to the calendar.
+     */
+    public void testAddEvent() {
+
+    }
+
+
+    /**
+     * Tests editing an existing event.
+     */
+    public void testEditEvent() {
+
+    }
+
+
+    /**
+     * Tests deleting an event.
+     */
+    public void testDeleteEvent() {
+
+    }
+
+
+    /**
+     * Tests viewing all events.
+     */
+    public void testViewAll() {
+
+    }
+
+
+    /**
+     * Tests viewing events on a specific date.
+     */
+    public void testViewByDate() {
+
+    }
+
+
+    /**
+     * Tests viewing events within a seven-day period.
+     */
+    public void testViewByWeek() {
+
+    }
+
+
+    /**
+     * Tests sorting events by priority.
+     */
+    public void testSortByPriority() {
+
+    }
+
+
+    /**
+     * Tests filtering events by category.
+     */
+    public void testFilterByCategory() {
+
+    }
+
+
+    /**
+     * Tests setting a category description.
+     */
+    public void testSetCategoryDescription() {
+
+    }
+
+
+    /**
+     * Tests retrieving a category description.
+     */
+    public void testGetCategoryDescription() {
+
+    }
+
+
+    /**
+     * Tests checking for events that are currently due.
+     */
+    public void testCheckDueNow() {
+
+    }
+
+
+    /**
+     * Tests loading standard U.S. holidays.
+     */
+    public void testLoadHolidays() {
+
+    }
+}
