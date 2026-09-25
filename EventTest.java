0a9cf4f7d25a82207ee6951efafa68a7b8fe32a1
zxcchoo080106 <cschoo7618@gmail.com>
2026-09-25 02:20:00 -0400

    Merge branch 'main' of https://github.com/jcvjr/CS2114-Project1-group99_.git into main

--- EventTest.java
+++ EventTest.java
@@ -7,65 +7,163 @@ import student.TestCase;
  * of the Event class to ensure that they behave as expected.
  * 
  * @author Xinchen Zhou
- * @version Sep 24, 2026
+ * @version Sep 25, 2026
  */
 
 
 public class EventTest extends student.TestCase
 {
     //~ Fields ................................................................
-
+    
+    private Event event;
+    private Event allDayEvent;
+    private Event priorityEvent;
+    private Event  holidayEvent;
+    private LocalDateTime dateTime;
+    
     //~ Constructors ..........................................................
+    
+    public EventTest()
+    {
+        
+    }
 
     //~Public  Methods ........................................................
     public void setUp()
     {
-        
+        dateTime = LocalDateTime.of(2026, 9, 24, 10, 30);
+        event = new Event(dateTime, "Meeting", 60);
+        //for covering 1st constructor
+        allDayEvent = new Event(dateTime, "Holiday", true); 
+        //for covering 2nd constructor
+        priorityEvent = new Event(dateTime, "Exam", 120, "School", 4); 
+        //for covering 3rd constructor
+        holidayEvent = new Event(dateTime, "Christmas", true, "Holiday", 5);
+        //for covering 4th constructor
     }
     
     
     //test methods for getters
     /**
-     * 
+     * test class for getendtime()
      * */
     public void testGetEndTime()
     {
-        
+        LocalDateTime expected =LocalDateTime.of(2026, 9, 24, 11, 30);
+        assertEquals(expected, event.getEndTime());
     }
     
-
+    /**
+     * test class for getdatetime()
+     * */
     public void testGetDateTime()
     {
-        
+        assertEquals(dateTime, event.getDateTime());
     }
     
-    
+    /**
+     * test class for getName()
+     * */
     public void testGetName()
     {
-        
+        assertEquals("Meeting", event.getName());
     }
     
+    /**
+     * test class for getDuration() when the event is not an all day event
+     * */
+    public void testGetDurationWhenNotAllDay()
+    {
+        assertEquals(60, event.getDuration());
+    }
     
-    public void testDuration()
+    /**
+     * test class for getDuration() when the event is an all day event
+     * */
+    public void testGetDurationWhenAllDay()
     {
         
+        assertEquals(0, allDayEvent.getDuration());
     }
     
-    
-    public void testCategory()
+    /**
+     * test class for getCategory() 
+     * */
+    public void testGetCategory()
     {
-        
+        assertEquals("Holiday", holidayEvent.getCategory());
     }
     
-    
+    /**
+     * test class for getPriority()
+     * */
     public void testGetPriority()
     {
-        
+        assertEquals(4, priorityEvent.getPriority());
+    }
+    
+    /**
+     * test class for getAllDay()
+     * */
+    public void testGetAllDay()
+    {
+        assertTrue(allDayEvent.getAllDay());
     }
     
     
-    public void testGetAllDay()
+  //test methods for setters, using getters for the methods returning void
+    /**
+     * test class for setDate()
+     * */
+    public void testSetDate()
     {
-        
+        LocalDateTime newDateTime = LocalDateTime.of(2026, 10, 1, 14, 0);
+        event.setDate(newDateTime);
+        assertEquals(newDateTime, event.getDateTime());
+    }
+    
+    /**
+     * test class for setName()
+     * */
+    public void testSetName()
+    {
+        event.setName("New Meeting");
+        assertEquals("New Meeting", event.getName());
+    }
+    
+    /**
+     * test class for setDuration()
+     * */
+    public void testSetDuration()
+    {
+        event.setDuration(90);
+        assertEquals(90, event.getDuration());
+    }
+    
+    /**
+     * test class for setCategory()
+     * */
+    public void testSetCategory()
+    {
+        event.setCategory("Work");
+        assertEquals("Work", event.getCategory());
+    }
+    
+    /**
+     * test class for setPriority()
+     * */   
+    public void testSetPriority()
+    {
+        event.setPriority(4);
+        assertEquals(4, event.getPriority());
+    }
+    
+    /**
+     * test class for setAllDay()
+     * */
+    public void testSetAllDay()
+    {
+        event.setAllDay(true);
+        assertTrue(event.getAllDay());
     }
 }
