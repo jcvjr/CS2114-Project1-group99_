04bbd033d264ba8cd97866bbcc21957046f4769b
Nick Marchese <ywgnick@gmail.com>
2026-09-25 12:42:55 -0400

    fixed

--- EventTest.java
+++ EventTest.java
@@ -1,5 +1,4 @@
-import java.time.*;
-import student.TestCase;
+import java.time.LocalDateTime;
 
 // -------------------------------------------------------------------------
 /**
@@ -10,175 +9,170 @@ import student.TestCase;
  * @version Sep 25, 2026
  */
 
+public class EventTest extends student.TestCase {
+    // ~ Fields ................................................................
 
-public class EventTest extends student.TestCase
-{
-    //~ Fields ................................................................
-    
     private Event event;
     private Event allDayEvent;
     private Event priorityEvent;
-    private Event  holidayEvent;
+    private Event holidayEvent;
     private LocalDateTime dateTime;
-    
-    //~ Constructors ..........................................................
-    
+
+    // ~ Constructors ..........................................................
+
     // ----------------------------------------------------------
     /**
      * Create a new EventTest object.
      */
-    public EventTest()
-    {
-        
+    public EventTest() {
+
     }
 
-    //~Public  Methods ........................................................
-    public void setUp()
-    {
+
+    // ~Public Methods ........................................................
+    public void setUp() {
         dateTime = LocalDateTime.of(2026, 9, 24, 10, 30);
         event = new Event(dateTime, "Meeting", 60);
-        //for covering 1st constructor
-        allDayEvent = new Event(dateTime, "Holiday", true); 
-        //for covering 2nd constructor
-        priorityEvent = new Event(dateTime, "Exam", 120, "School", 4); 
-        //for covering 3rd constructor
+        // for covering 1st constructor
+        allDayEvent = new Event(dateTime, "Holiday", true);
+        // for covering 2nd constructor
+        priorityEvent = new Event(dateTime, "Exam", 120, "School", 4);
+        // for covering 3rd constructor
         holidayEvent = new Event(dateTime, "Christmas", true, "Holiday", 5);
-        //for covering 4th constructor
+        // for covering 4th constructor
     }
-    
-    
-    //test methods for getters
+
+
+    // test methods for getters
     /**
      * test class for getEndTime()
-     * */
-    public void testGetEndTime()
-    {
-        LocalDateTime expected =LocalDateTime.of(2026, 9, 24, 11, 30);
+     */
+    public void testGetEndTime() {
+        LocalDateTime expected = LocalDateTime.of(2026, 9, 24, 11, 30);
         assertEquals(expected, event.getEndTime());
     }
-    
+
+
     /**
      * test class for getDateTime()
-     * */
-    public void testGetDateTime()
-    {
+     */
+    public void testGetDateTime() {
         assertEquals(dateTime, event.getDateTime());
     }
-    
+
+
     /**
      * test class for getName()
-     * */
-    public void testGetName()
-    {
+     */
+    public void testGetName() {
         assertEquals("Meeting", event.getName());
     }
-    
+
+
     /**
      * test class for getDuration() when the event is not an all day event
-     * */
-    public void testGetDurationWhenNotAllDay()
-    {
+     */
+    public void testGetDurationWhenNotAllDay() {
         assertEquals(60, event.getDuration());
     }
-    
+
+
     /**
      * test class for getDuration() when the event is an all day event
-     * */
-    public void testGetDurationWhenAllDay()
-    {
-        
+     */
+    public void testGetDurationWhenAllDay() {
+
         assertEquals(0, allDayEvent.getDuration());
     }
-    
+
+
     /**
-     * test class for getCategory() 
-     * */
-    public void testGetCategory()
-    {
+     * test class for getCategory()
+     */
+    public void testGetCategory() {
         assertEquals("Holiday", holidayEvent.getCategory());
     }
-    
+
+
     /**
      * test class for getPriority()
-     * */
-    public void testGetPriority()
-    {
+     */
+    public void testGetPriority() {
         assertEquals(4, priorityEvent.getPriority());
     }
-    
+
+
     /**
      * test class for getAllDay()
-     * */
-    public void testGetAllDay()
-    {
+     */
+    public void testGetAllDay() {
         assertTrue(allDayEvent.getAllDay());
     }
-    
-    
-  //test methods for setters, using getters for the methods returning void
+
+
+    // test methods for setters, using getters for the methods returning void
     /**
      * test class for setDate()
-     * */
-    public void testSetDate()
-    {
+     */
+    public void testSetDate() {
         LocalDateTime newDateTime = LocalDateTime.of(2026, 10, 1, 14, 0);
         event.setDate(newDateTime);
         assertEquals(newDateTime, event.getDateTime());
     }
-    
+
+
     /**
      * test class for setName()
-     * */
-    public void testSetName()
-    {
+     */
+    public void testSetName() {
         event.setName("New Meeting");
         assertEquals("New Meeting", event.getName());
     }
-    
+
+
     /**
      * test class for setDuration()
-     * */
-    public void testSetDuration()
-    {
+     */
+    public void testSetDuration() {
         event.setDuration(90);
         assertEquals(90, event.getDuration());
     }
-    
+
+
     /**
      * test class for setCategory()
-     * */
-    public void testSetCategory()
-    {
+     */
+    public void testSetCategory() {
         event.setCategory("Work");
         assertEquals("Work", event.getCategory());
     }
-    
+
+
     /**
      * test class for setPriority()
-     * */   
-    public void testSetPriority()
-    {
+     */
+    public void testSetPriority() {
         event.setPriority(4);
         assertEquals(4, event.getPriority());
     }
-    
+
+
     /**
      * test class for setAllDay()
-     * */
-    public void testSetAllDay()
-    {
+     */
+    public void testSetAllDay() {
         event.setAllDay(true);
         assertTrue(event.getAllDay());
     }
-    
+
+
     // ----------------------------------------------------------
     /**
      * Place a description of your method here.
      */
-    public void testToString()
-    {
-        Event dogWalking =
-            new Event(dateTime, "Dog Walking", 30, "Activity", 4);
+    public void testToString() {
+        Event dogWalking = new Event(dateTime, "Dog Walking", 30, "Activity",
+            4);
         String expected =
             "Dog Walking on 2026-09-24 at 10:30 AM (Activity, priority 4)";
         assertEquals(expected, dogWalking.toString());
